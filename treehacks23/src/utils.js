7005de59d238dfbea27eab39ff5b19a57c0699c0
Alex Cho <104597806+aychocho@users.noreply.github.com>
2023-02-18 03:47:25 -0500

    test minting api

--- treehacks23/src/utils.js
+++ treehacks23/src/utils.js
@@ -1,4 +1,4 @@
-import { AptosAccount, AptosClient } from 'aptos'
+import { AptosAccount, AptosClient, TokenClient } from 'aptos'
 
 
 /**
@@ -7,13 +7,30 @@ import { AptosAccount, AptosClient } from 'aptos'
  * Aptos Devnet API Docs: https://fullnode.devnet.aptoslabs.com/v1/spec#/operations/get_account
  *  
  */
+const admin_address = "0x76c6703811ecfc91ca761600b782ae3cd1a9845c3ca940f14225a491d64213e7"
 
 const defaultNetworks = {
     nodeUrl: 'https://fullnode.devnet.aptoslabs.com'
 }
 
+function getAptosClient() {
+    return new AptosClient(defaultNetworks.nodeUrl)
+}
+
 async function getAccountResources(address) {
-    const aptosClient = new AptosClient(defaultNetworks.nodeUrl)
+    const aptosClient = getAptosClient();
     const accountResources = await aptosClient.getAccountResources(address);
     return accountResources;
+}
+
+function createToken(address, name, SerialNum, description, img) {
+    const tokenClient = new TokenClient(getAptosClient());
+    tokenClient.createCollection(address, name, description, img, 1);
+    const hash = tokenClient.createToken(address, name, SerialNum, description, 1, img, 1);
+    return hash;
+}
+
+function sendToken(address, name, SerialNum, dest) {
+    const tokenClient = new TokenClient(getAptosClient());
+    tokenClient.offerToken(address, dest, admin_address, name, SerialNum, 1);
 }
